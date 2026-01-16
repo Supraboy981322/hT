2348e29cd6a706bcf426d475f6700ff5ce14583e
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-01-16 13:28:24 -0600

    set mimetype for js

--- init.go
+++ init.go
@@ -3,6 +3,7 @@ package main
 import(
 	"os"
 	"fmt"
+	"mime"
 	"slices"
 	"errors"
 	"strings"
@@ -10,6 +11,8 @@ import(
 )
 
 func init() {
+	mime.AddExtensionType(".js", "application/javascript")
+	mime.AddExtensionType(".mjs", "application/javascript")
 	read_config()
 	parse_args()
 }
